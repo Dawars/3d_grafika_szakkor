3d6687da7d44b1fd897a20ece427e7e836508581
dawars <dawars00@gmail.com>
2018-05-11 15:18:55 +0200

    Szakkor 8 - Metaballs

---
+++ src/main/resources/szakkor8/vert.glsl
@@ -0,0 +1,33 @@
+uniform mat4 transform;
+uniform mat4 modelview;
+uniform mat3 normalMatrix;
+
+uniform vec4 lightPosition;
+
+attribute vec4 position;
+attribute vec4 color;
+attribute vec3 normal;
+
+varying vec4 vertColor; // color
+varying vec3 lightDir; // light
+varying vec3 ecNormal; // normal
+varying vec3 ecPosition; // vert pos
+
+
+void main() {
+    // screen coordinate
+    gl_Position = transform * position;
+
+    // camera space
+    ecPosition = vec3(modelview * position); // eye coordinates
+
+    // vertex color
+    vertColor = color;
+
+    // normal
+    ecNormal = normalMatrix * normalize(normal);
+
+    // incident light
+    lightDir = lightPosition.xyz - ecPosition; // eye coordinates
+
+}
